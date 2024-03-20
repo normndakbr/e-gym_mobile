8ac0afdb7691a0f0495b740e069c3569f1d6fc53
Normandia Akbar <normnd.akbr2@gmail.com>
2024-03-20 14:06:00 +0800

    refactor layout

--- lib/src/pages/login.dart
+++ lib/src/pages/login.dart
@@ -1,35 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import '../widgets/custom_text_form_field.dart';
 
 class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Login Page'),
-      ),
       body: Padding(
         padding: EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            CustomTextFormField(
-              labelText: 'Email',
-            ),
-            SizedBox(height: 20),
-            CustomTextFormField(
-              labelText: 'Password',
-              obscureText: true,
-            ),
-            SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                // Perform login functionality here
-                print('Login button pressed');
-              },
-              child: Text('Login'),
-            ),
-          ],
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 50),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              CustomTextFormField(
+                labelText: 'Email / Member ID',
+              ),
+              SizedBox(height: 30),
+              CustomTextFormField(
+                labelText: 'Password',
+                obscureText: true,
+              ),
+              SizedBox(height: MediaQuery.of(context).size.height * 0.07),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  ElevatedButton(
+                    onPressed: () {
+                      print('Login button pressed');
+                    },
+                    child: Text('Login'),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
